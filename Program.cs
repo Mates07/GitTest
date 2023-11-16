a1641600b8437d32d6969fc15754bad3fdc16d6d
Matej Dolecek <1179@adc.itgymnazium.cz>
2023-11-16 09:44:34 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string e = Console.ReadLine();
+if (int.TryParse(e, out int f))
+{
+    if (int.TryParse(a, out int c))
+    {
+        if (int.TryParse(b, out int d))
+        {
+            for (int i = d; i <= c; i = i + f)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+*/
+string a = Console.ReadLine();
+for (int i = 0; i <= 4; i++)
+{
+    Console.WriteLine(a);
+}
